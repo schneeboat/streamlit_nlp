3127b305b403dda9f16c8cbe40a0f7790bfa04c3
Ludwig <xuezhou@Ludwig.home>
2021-01-21 16:05:15 +0100

    add

--- nlp.py
+++ nlp.py
@@ -4,15 +4,18 @@ import matplotlib.pyplot as plt
 import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
+nltk.download('vader_lexicon')
+from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from collections import Counter
-from wordcloud import WordCloud
+#from wordcloud import WordCloud
 import string
 
-
-input_text = st.sidebar.text_area(label="English text you want to analyze:", height = 400)
-input_num = st.sidebar.slider(label='How many words to keep (max)?', max_value=500)
+st.sidebar.title('Text analysis:')
+input_text = st.sidebar.text_area(label="English text you want to analyze:", height=300)
+     
+input_num = st.sidebar.slider(label='How many words to keep for wordcloud viz?', max_value=500)
 
 def cloud(text, number):
     
@@ -35,12 +38,52 @@ def cloud(text, number):
     fig = plt.figure(figsize=(10,5))
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
-    return fig
+    return st.puplot(fig)
 
-if cloud(input_text,input_num):
-    st.write(cloud(input_text,input_num))
+    
+    
+sia = SentimentIntensityAnalyzer()
+score = sia.polarity_scores(input_text)
 
+with st.beta_container():
+     st.header('Wordcloud image')
+     if not cloud(input_text,input_num):
+         st.warning('Please input the text and select a number :)')
+        
+        
+     if cloud(input_text,input_num):
+          st.write(cloud(input_text,input_num))
+     elif not input_num:
+              st.warning('Please select a number :)')
+     elif not input_text:
+              st.warning('Please input the text you want to analyze :)')
+      
+      
+      
+with st.beta_container():    
+    st.header('Sentiment analysis')
+    
+    if input_text:
+        fig2, ax = plt.subplots(figsize=(12,6))
+        ax.bar(*zip(*score.items()), color='rosybrown', bottom=0)
+        for p in ax.patches:
+            height = p.get_height()
+            ax.annotate(height,(p.get_x() + p.get_width() / 2., p.get_height()), 
+                ha='center', va='center', xytext=(0, 4.5), textcoords='offset points', color='black')
+        st.pyplot(fig2)
+    if not input_text:
+        st.warning('Please input the text you want to analyze :)')
 
-
-
-
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
