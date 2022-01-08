3847fa008803c69e46006b0460f7d80fc1b052db
Ludwig <xuezhou@Ludwig.local>
2022-01-08 23:12:51 +0100

    add

--- nlp.py
+++ nlp.py
@@ -18,7 +18,7 @@ import re
 import networkx as nx
 from remotezip import RemoteZip
 
-@st.cache
+#@st.cache
 #def load_remote():
  #   with RemoteZip('http://nlp.stanford.edu/data/glove.6B.zip') as zf:
   #      file = zf.extract('glove.6B.100d.txt')
